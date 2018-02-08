7dc5ed748411940381d3163386e56584d549f5ed
Benjamin Tatum <benjamintatum@Benjamins-MacBook-Pro-3.local>
2018-02-08 15:04:11 -0500

    2.0.0: improved defaults. key works as a string.

--- src/collectionToHash.js
+++ src/collectionToHash.js
@@ -2,8 +2,8 @@
 export default (collection = [], key, val) => {
   const res = {}
   collection.forEach(x => {
-    const k = typeof key === 'function' ? key(x) : x.key
-    const v = typeof val === 'function' ? val(x) : x.value
+    const k = typeof key === 'function' ? key(x) : x[key]
+    const v = typeof val === 'function' ? val(x) : x
     res[k] = v
   })
   return res
